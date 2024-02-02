a55b49ed0f9ad3e3d0eea8a4fa08ee75afb41a44
Nicklas Jonsson <jonsson.nicklas1990@gmail.com>
2024-02-03 00:25:09 +0100

    update structure

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-from setuptools import setup
-
-
+# https://stackoverflow.com/questions/50585246/pip-install-creates-only-the-dist-info-not-the-package
+from setuptools import setup, find_packages
 
 setup(
     name='brain2lib',
@@ -10,7 +9,7 @@ setup(
     author='Samlarn',
     author_email='samlarn@example.com',
     license='MIT',
-    packages=['src/brain2lib'],
+    packages= find_packages(), #['src/brain2lib'],
     install_requires=[
         "SQLAlchemy==2.0.25",
     ],
