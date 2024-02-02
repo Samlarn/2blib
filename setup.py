1327beca5525aac4f97f82ec80e6b33470d6d121
Nicklas Jonsson <jonsson.nicklas1990@gmail.com>
2024-02-02 23:52:05 +0100

    setup url

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ setup(
     name='brain2lib',
     version='0.0.1',
     description='brain2 package',
-    url='http://github.com/storborg/funniest',
+    url='git@github.com:Samlarn/2blib.git',
     author='Samlarn',
     author_email='samlarn@example.com',
     license='MIT',
     packages=['src/brain2lib'],
-    zip_safe=False,
     install_requires=[
         "SQLAlchemy==2.0.25",
-    ]
+    ],
+    zip_safe=False
 )
