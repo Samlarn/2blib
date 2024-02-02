8104491161be9242da3384e6c6add10e35f6a1e5
Nicklas Jonsson <jonsson.nicklas1990@gmail.com>
2024-02-02 23:35:08 +0100

    update setup

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from setuptools import setup
 
+
+
 setup(
+    name='brain2lib',
+    version='0.0.1',
+    description='brain2 package',
+    url='http://github.com/storborg/funniest',
+    author='Samlarn',
+    author_email='samlarn@example.com',
+    license='MIT',
+    packages=['src/brain2lib'],
+    zip_safe=False,
     install_requires=[
         "SQLAlchemy==2.0.25",
     ]
